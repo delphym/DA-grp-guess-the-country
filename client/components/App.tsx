96317a0eb1a3797c06fd2b8eb0eef37697058256
delphym <dmladek@gmail.com>
2024-10-17 20:10:45 +1300

    Added basic CSS styling and example of use into App component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,30 @@
 function App() {
   return (
     <div>
-      <h1>App</h1>
-      <p>React development has begun!</p>
+      <nav>This is NAV bar</nav>
+      <main>
+        <h1>Guess the Country</h1>
+        <p>React development has begun!</p>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="http://delphym-mania-chc-24-worldwide-routing.devacademy.nz/">
+              Need some learning?
+            </a>
+          </li>
+          <li>
+            <a href="#credit">Credit</a>
+          </li>
+        </ul>
+      </main>
+      <footer id="credit">
+        This game is proudly <b>Brought to You by The-A-Team</b>!
+        <div>
+          <img src="/images/a-team-smile.gif" alt="The-A-Team" />
+        </div>
+      </footer>
     </div>
   )
 }
