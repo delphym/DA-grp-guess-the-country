91053ffdb22117da5c4498ef3165aef4b53c9a8c
Riley Wessel <lessewyendor@gmail.com>
2024-10-17 17:40:33 +1300

    Get flag image from external site

--- client/components/Flag.tsx
+++ client/components/Flag.tsx
@@ -1,3 +1,5 @@
 function Flag(code: string) {
-    return <img src={`/images/flag-${code}.webp` alt="Flag, but you gotta guess which one"}></img>
+    return <img src={`https://flagpedia.net/data/flags/w580/${code.toLowerCase()}.webp`} alt={`Flag of country with code '${code}'`}></img>
 }
+
+export default Flag
