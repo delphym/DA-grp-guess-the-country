7a93941fbb74906954df4155b751544fba1499c2
DelphyM <dmladek@gmail.com>
2024-10-18 18:44:58 +1300

    Moved all the content out from the App to the Main component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,63 +1,13 @@
-import { useState } from 'react'
 import Nav from './Nav'
 import Main from './Main'
-import Game from './Game'
-import GuessForm from './GuessForm'
-import Flag from './Flag'
-import Score from './Score'
 import Footer from './Footer'
-import data from '../../data/countries'
 
 function App() {
-  const [showGame, setShowGame] = useState(false) // State to manage Game component visibility
-
-  const handleShowGame = () => {
-    event?.preventDefault()
-    setShowGame(!showGame) // Toggle the visibility of the Game component
-  }
-  console.log(showGame)
   return (
     <div>
       <Nav />
-
       <main>
-        <h1>Guess the Country</h1>
-        {/* <p>React development has begun!</p> */}
-        <ul>
-          <li>
-            <a href="/">| Home |</a>
-          </li>
-
-          <li>
-            <a href="http://delphym-mania-chc-24-worldwide-routing.devacademy.nz/">
-              | Need some learning? |
-            </a>
-          </li>
-          <li>
-            <a href="#credit">| Credit |</a>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <button id="gamebtn" onClick={handleShowGame}>
-              {showGame ? 'Hide Game' : 'Show Game'}
-            </button>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            {/* <Score />  */}
-            {/* <p id="score"></p> */}
-            {/* <p id="score">0</p> */}
-          </li>
-        </ul>
-        {/* <GuessForm/> */}
-        {showGame && <Game />}
-
-        {/* <Main /> */}
-        {/* <Flag handleFlag={handleFlag} /> */}
-        {/* <Score score={1} countryName={'New Zealand'} countryCode={'NZ'} /> */}
-        {/* <GuessForm handleCountryName={handleCountryName} /> */}
+        <Main />
       </main>
       <footer id="credit">
         <Footer />
--- client/components/Main.tsx
+++ client/components/Main.tsx
@@ -1,22 +1,44 @@
+import { useState } from 'react'
+import Game from './Game'
+
 function Main() {
+  const [showGame, setShowGame] = useState(false) // State to manage Game component visibility
+
+  const handleShowGame = () => {
+    event?.preventDefault()
+    setShowGame(!showGame) // Toggle the visibility of the Game component
+  }
+  console.log(showGame)
+
   return (
     <>
       <h1>Guess the Country</h1>
-      <p>React development has begun!</p>
-
       <ul>
         <li>
-          <a href="/">Home</a>
+          <a href="/">| Home |</a>
         </li>
+
         <li>
           <a href="http://delphym-mania-chc-24-worldwide-routing.devacademy.nz/">
-            Need some learning?
+            | Need some learning? |
           </a>
         </li>
         <li>
-          <a href="#credit">Credit</a>
+          <a href="#credit">| Credit |</a>
         </li>
       </ul>
+      <ul>
+        <li>
+          <button id="gamebtn" onClick={handleShowGame}>
+            {showGame ? 'Hide Game' : 'Show Game'}
+          </button>
+        </li>
+      </ul>
+      <ul>
+        <li></li>
+      </ul>
+      {/* <GuessForm/> */}
+      {showGame && <Game />} {/* Render the Game component */}
     </>
   )
 }
